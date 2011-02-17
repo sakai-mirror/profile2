ee3b462f28196754d3c73c2db5c08c27e4e769a9
steve.swinsburg@gmail.com <steve.swinsburg@gmail.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2011-02-17 02:04:44 +0000

    PRFL-432 merge trunk r87986 and r88485
    git-svn-id: https://source.sakaiproject.org/svn/profile2/branches/profile2-1.3.x@88489 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- tool/src/java/org/sakaiproject/profile2/tool/components/PhoneNumberValidator.java
+++ tool/src/java/org/sakaiproject/profile2/tool/components/PhoneNumberValidator.java
@@ -18,6 +18,21 @@ import org.apache.wicket.validation.validator.AbstractValidator;
  *	5. 1234 5678 (if calling in 02 areacode) <br /><br />
  *  
  *  plus all of the above without spaces.</p>
+ *  
+ *  <p>Also added the following style numbers:<br />
+ *  6. 111-222-3333 <br />
+ *  7. 111.222.3333 <br />
+ *  8. (111) 222-3333-444 <br />
+ *  9. (111) 222.3333.444 <br /><br />
+ *  
+ *  plus combinations of . and - in the same numbers above</p>
+ *  
+ *  <p>Now for numbers with extensions, add (\\w+)?+ 
+ *  10. All of the above with any letter/number combo afterwards but it must start with a letter and not contain spaces and only occur once or not at all
+ *   eg (123-456-789 x123, 123 456 789 ext5)
+ *  
+ *  </p>
+ *  
  * 
  * @author Steve Swinsburg (steve.swinsburg@gmail.com)
  *
@@ -33,9 +48,11 @@ public class PhoneNumberValidator extends AbstractValidator {
 	{
 		//setup list
 		List<String> regexs = new ArrayList<String>();
-		regexs.add("\\+?([0-9]+|\\s+)+"); //matches 1,2,3,5
-		regexs.add("\\({1}[0-9]+\\){1}([0-9]+|\\s+)+"); //matches 4
-
+		regexs.add("\\+?([0-9]+|\\s+)+(\\w+)?+"); //matches 1,2,3,5 with 10
+		regexs.add("\\({1}[0-9]+\\){1}([0-9]+|\\s+)+(\\w+)?+"); //matches 4 with 10
+		regexs.add("([0-9]+(\\-|\\.)?)+(\\w+)?+"); //matches 6, 7
+		regexs.add("\\({1}[0-9]+\\){1}([0-9]+|\\s+|\\-?|\\.?)+(\\w+)?+"); //matches 8,9
+		
 		//check each, if none, error
 		for(String r: regexs) {
 			Pattern p = Pattern.compile(r);
