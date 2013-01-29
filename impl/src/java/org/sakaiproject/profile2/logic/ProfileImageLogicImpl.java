c7f113e81f4d2c613c390c9762d0c2c3fdfbee61
steve.swinsburg@gmail.com <steve.swinsburg@gmail.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2013-01-29 00:03:35 +0000

    PRFL-747 merge trunk 109444
    git-svn-id: https://source.sakaiproject.org/svn/profile2/branches/profile2-1.4.x@118930 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- impl/src/java/org/sakaiproject/profile2/logic/ProfileImageLogicImpl.java
+++ impl/src/java/org/sakaiproject/profile2/logic/ProfileImageLogicImpl.java
@@ -177,7 +177,7 @@ public class ProfileImageLogicImpl implements ProfileImageLogic {
 			break;
 			
 			case ProfileConstants.PICTURE_SETTING_OFFICIAL: 
-				image = getOfficialImage(currentUserUuid,image,defaultImageUrl,isSameUser);
+				image = getOfficialImage(userUuid,image,defaultImageUrl,isSameUser);
 			break;
 			
 			default:
